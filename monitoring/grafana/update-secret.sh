1b9779fe1d7ce28890638a9dfa46f27c1dbe8c92
Tomas Edwardsson <tommi@tommi.org>
2025-03-01 15:25:51 +0000

    Revert to older grafana version

--- monitoring/grafana/update-secret.sh
+++ monitoring/grafana/update-secret.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-DB_URL=$(kubectl get secret cxs-pg-pguser-grafana-db --namespace data -o jsonpath='{.data.pgbouncer-uri}' | base64 --decode | sed 's/postgresql:/postgres:/')
+DB_URL=$(kubectl get secret cxs-pg-pguser-grafana --namespace data -o jsonpath='{.data.pgbouncer-uri}' | base64 --decode | sed 's/postgresql:/postgres:/')
 echo "$DB_URL"
 awk -v db_url="$DB_URL" '{gsub(/__DB_URL__/, db_url); print}' secret.yaml.tmpl | kubectl -n grafana apply -f -
